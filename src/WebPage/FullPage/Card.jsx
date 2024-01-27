3615f53a7bbd794720e21d78cbd2b57025f31c04
Muzammil <muzammil809040@gmail.com>
2024-01-28 02:39:37 +0530

    Scrolling Task Completed

--- src/WebPage/FullPage/Card.jsx
+++ src/WebPage/FullPage/Card.jsx
@@ -16,9 +16,9 @@ const Card = () => {
   };
   const submmit = () => {
     if (isValid) {
-      <p>Password is valid!</p>;
+      alert("Congratulation! Passward is strong !");
     } else {
-      <p>Password is invalid.</p>;
+      alert("Sorry! Passward is too week !");
     }
   };
 
@@ -313,23 +313,50 @@ const Card = () => {
         </div>
         <div className="mainContent">
           <h1>Daily Summery</h1>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
-          minima eaque, eum quo quaerat quidem commodi, asperiores cumque
-          perferendis architecto, aliquam iste repellendus earum. Reiciendis non
-          tempora eum error debitis ullam eaque! Quod nulla iusto illo ipsam est
-          omnis, amet ab expedita suscipit earum rerum laborum placeat obcaecati
-          adipisci aliquam architecto doloremque? Iusto aperiam error nisi
-          cupiditate quia officiis? Aspernatur libero, fugit dolores sunt
-          repellendus dolor nesciunt quod deserunt repellat quo veritatis eaque
-          ad in fugiat esse quidem nobis. Cupiditate autem, quod dicta minus
-          vero perspiciatis voluptatibus rem officia ipsum quos doloremque
-          mollitia similique obcaecati porro impedit repellat reprehenderit aut
-          debitis non optio corrupti. Obcaecati nesciunt rerum dolorem alias ut
-          repellat assumenda, quae necessitatibus exercitationem. Reiciendis,
-          velit. Delectus placeat id suscipit enim nulla, ad optio voluptate
-          saepe odio laborum natus? Reiciendis ipsam saepe cumque assumenda
-          autem amet explicabo, ex quas aspernatur voluptates optio provident
-          hic doloribus, earum molestias aliquid porro esse minima voluptatibus
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iusto
+          voluptatem nam laboriosam temporibus aperiam dolor, totam beatae,
+          officia magni quisquam odit cupiditate accusamus, ut placeat explicabo
+          possimus animi eveniet. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Ab tenetur dolores voluptate quasi, aperiam minus
+          laboriosam fugit, velit animi, non quia totam provident harum rerum
+          earum commodi quod quam tempora doloremque minima aliquam iure
+          aspernatur nostrum. Repellendus doloribus ducimus aliquid qui quo
+          vitae sapiente maxime beatae? Reprehenderit, omnis quod. Molestias
+          facilis qui suscipit debitis aliquid temporibus tempore, possimus in
+          necessitatibus. Repellat officiis excepturi possimus maxime
+          reiciendis. Laudantium, odio. Nulla voluptatum illo laborum aut
+          provident mollitia possimus dolorum, cumque voluptatibus dolores ea
+          eligendi soluta modi unde ratione dignissimos omnis temporibus enim
+          in, necessitatibus explicabo saepe. Suscipit, nisi maxime recusandae
+          consequuntur aliquid quasi! Soluta quos rem illo laborum, magnam
+          facilis quasi maiores error corporis, quis consectetur numquam
+          pariatur neque sit iusto architecto voluptate, ullam optio? Provident
+          iste dolorem itaque repellendus earum consequuntur eos ab aut! Laborum
+          quod sint ipsum eius eum omnis aliquam consequatur, maiores qui aut
+          inventore magni quaerat beatae officiis ex vel. Doloribus blanditiis
+          repudiandae quae, exercitationem debitis laborum? Soluta iure cumque
+          nostrum eos facilis assumenda nobis suscipit modi vel veritatis
+          laborum, eligendi voluptatem sint voluptates quis maiores ea
+          consequatur aspernatur fugiat iste aut quas. Animi porro at ab
+          veritatis adipisci, molestiae doloremque? Provident ut commodi quidem
+          doloribus a necessitatibus! Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Obcaecati minima eaque, eum quo quaerat quidem
+          commodi, asperiores cumque perferendis architecto, aliquam iste
+          repellendus earum. Reiciendis non tempora eum error debitis ullam
+          eaque! Quod nulla iusto illo ipsam est omnis, amet ab expedita
+          suscipit earum rerum laborum placeat obcaecati adipisci aliquam
+          architecto doloremque? Iusto aperiam error nisi cupiditate quia
+          officiis? Aspernatur libero, fugit dolores sunt repellendus dolor
+          nesciunt quod deserunt repellat quo veritatis eaque ad in fugiat esse
+          quidem nobis. Cupiditate autem, quod dicta minus vero perspiciatis
+          voluptatibus rem officia ipsum quos doloremque mollitia similique
+          obcaecati porro impedit repellat reprehenderit aut debitis non optio
+          corrupti. Obcaecati nesciunt rerum dolorem alias ut repellat
+          assumenda, quae necessitatibus exercitationem. Reiciendis, velit.
+          Delectus placeat id suscipit enim nulla, ad optio voluptate saepe odio
+          laborum natus? Reiciendis ipsam saepe cumque assumenda autem amet
+          explicabo, ex quas aspernatur voluptates optio provident hic
+          doloribus, earum molestias aliquid porro esse minima voluptatibus
           recusandae? Porro in voluptate voluptas minus totam ipsa, nihil
           laborum eaque deleniti cupiditate! Modi ex consequuntur, tempore
           mollitia ipsum unde magni veritatis rem consectetur nam vitae possimus
@@ -348,11 +375,6 @@ const Card = () => {
               <input type="password" value={password} onChange={handelChange} />
               <input type="button" value="Submmit" onClick={submmit} />
               <br /> <br />
-              {isValid ? (
-                <p>Password is valid!</p>
-              ) : (
-                <p>Password is invalid.</p>
-              )}
               <h5>Forgrt Password?</h5> <br />
               <p>
                 <h5>Note:</h5>
--- src/WebPage/FullPage/Header.jsx
+++ src/WebPage/FullPage/Header.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Header.css";
 import HindustanTimesLogo from "../logo/HindustanLogo.png";
-const Header = () => {
-  return (
-    <>
-      <div className="container">
-        <ul className="list-1">
-          <li>Home</li>
-          <li>About-Us</li>
-        </ul>
-        <img src={HindustanTimesLogo} alt="" className="hindustanTimesLogo" />
 
-        <ul className="list-2">
-          <li>Contact-Us</li>
-          <li>Search</li>
-          <li>Carriers</li>
-          <li>Content</li>
-        </ul>
-      </div>
-    </>
+const Header = () => {
+  const task = useRef("");
+
+  useEffect(() => {
+    const funn = () => {
+      console.log("I am sorolling !");
+      if (task.current.style) {
+        if (window.scrollY > 15) {
+          task.current.style.opacity = 0;
+          task.current.style.transition = "opacity 0.5s ease-in-out";
+        } else {
+          task.current.style.opacity = 1;
+          task.current.style.transition = "opacity 0.1s ease";
+        }
+      }
+    };
+
+    window.addEventListener("scroll", funn);
+
+    return () => {
+      window.removeEventListener("scroll", funn);
+    };
+  }, []);
+
+  return (
+    <div className="container" ref={task}>
+      <ul className="list-1">
+        <li>Home</li>
+        <li>About-Us</li>
+      </ul>
+      <img src={HindustanTimesLogo} alt="" className="hindustanTimesLogo" />
+
+      <ul className="list-2">
+        <li>Contact-Us</li>
+        <li>Search</li>
+        <li>Carriers</li>
+        <li>Content</li>
+      </ul>
+    </div>
   );
 };
 
